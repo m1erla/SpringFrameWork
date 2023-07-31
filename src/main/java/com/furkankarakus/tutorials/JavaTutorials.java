ddae2216bfba9f7ae7a7fca2f38f0ed12ab5d51e
Furkan Karakus <furkankarakus01@hotmail.com>
2023-07-31 22:08:30 +0300

    Java High Level reminder

---
+++ src/main/java/com/furkankarakus/tutorials/JavaTutorials.java
@@ -0,0 +1,61 @@
+package com.furkankarakus.tutorials;
+
+public class JavaTutorials {
+    //        Arrays.sort();
+//        this.
+//        super
+//        break continue return
+//        optional
+    //cast
+    //primitive type
+    //wrapper class
+    //Random
+    //Math
+    // %s %d %f ==> printf()
+    //final
+    //static
+    //decoder encoder
+    //instanceof
+    //public private protected
+    //String
+    //StringBuilder
+    //StringTokenizer
+    //StringBuffer
+    //Enum
+    //Multithread
+
+    //S.O.L.I.D
+    //KISS
+    //DRY
+    //MONAD
+    //List Set Map
+    //Liste:LinkedList ArrayList
+    //Generic <>
+    //Iterator
+    //OOP
+    //interface
+    //abstract new
+    //inheritance
+
+    //UML - + __
+    //class
+    //POJO
+    //Bean
+    //Serileştirme
+    //IO
+    //JAR WAR
+    //Dağıtık
+    //server
+
+    //constructor
+    //excepiton
+    //error
+
+    //Java 8 : stream,map,filter,sort
+    //Optional
+    //Lambda Expression
+    //Method Referances ::
+
+    //D.P.
+
+}
